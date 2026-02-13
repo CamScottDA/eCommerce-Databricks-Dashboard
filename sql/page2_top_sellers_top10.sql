7f2a19494c7393079ea044e0581364af107c6f8c
CamScottDA <legacyredev@gmail.com>
2026-02-13 13:39:33 -0800

    Update page2_top_sellers_top10.sql

--- sql/page2_top_sellers_top10.sql
+++ sql/page2_top_sellers_top10.sql
@@ -4,16 +4,15 @@
 -- Sources: ecom_mart.v_top_sellers_monthly
 
 SELECT
-  month_start,
-  seller_id,
+  CAST(month_start AS DATE) AS month_start_date,
+  seller_rank,
   seller_name,
   seller_country,
   revenue,
   orders,
   items,
   avg_review_score,
-  late_rate_among_delivered,
-  seller_rank
+  late_rate_among_delivered
 FROM ecom_mart.v_top_sellers_monthly
-WHERE seller_rank <= 10
-ORDER BY seller_rank;
+ORDER BY month_start_date, seller_rank;
+
